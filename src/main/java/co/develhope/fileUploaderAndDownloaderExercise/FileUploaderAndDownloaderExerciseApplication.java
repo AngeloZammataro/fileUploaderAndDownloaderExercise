b20bb8a13092dbd89e409d2b68c5de86fd773560
Angelo Zammataro <juba.kem@gmail.com>
2022-08-10 20:55:42 +0200

    First Commit: Servizi - File Upload
                  Exercise: file-upload-and-download

---
+++ src/main/java/co/develhope/fileUploaderAndDownloaderExercise/FileUploaderAndDownloaderExerciseApplication.java
@@ -0,0 +1,24 @@
+/*
+Servizi - File Upload
+
+Exercise: file-upload-and-download
+
+    create a spring boot project
+    create a controller REST
+    Create a upload endpoint to upload a file
+    Create a download controller to download the previouslly uploaded file
+ */
+
+package co.develhope.fileUploaderAndDownloaderExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploaderAndDownloaderExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploaderAndDownloaderExerciseApplication.class, args);
+	}
+
+}
